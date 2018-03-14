bf4922df08e1def3ff4a36b3fb0c47e146f07b4a
Bennett Hardwick (PC) <bennetthardwick@icloud.com>
2018-03-14 22:27:37 +1000

    Added skeleton methods for database

---
+++ db.js
@@ -0,0 +1,25 @@
+const sqlite3 = require('sqlite3').verbose();
+
+module.exports.addPing = (device_id, epoch_time) => {
+
+};
+
+module.exports.getPingByDate = (device_id, date) => {
+
+};
+
+module.exports.getPingByPeriod = (device_id, from, to) => {
+
+};
+
+module.exports.getDevices = () => {
+
+};
+
+module.exports.resetDatabase = () => {
+
+};
+
+module.exports.createTables = () => {
+
+};
