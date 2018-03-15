816913fc28cd6e91b5f06b198835de76c0295857
Bennett Hardwick (PC) <bennetthardwick@icloud.com>
2018-03-15 13:36:29 +1000

    Added tests to test parseTime functions

--- spec/db.spec.js
+++ spec/db.spec.js
@@ -20,4 +20,12 @@ describe('Database', () => {
       })
   });
 
+  it('parses time strings correctly', () => {
+    expect(database.parseTime('2018-01-01') == 1514764800).to.equal(true);
+  });
+
+  it('parses unix timestamp correctly', () => {
+    expect(database.parseTime('1514764800') == 1514764800).to.equal(true);
+  })
+
 });
