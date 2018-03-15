d2835394a690e32dca140d9d205bb171146beded
Bennett Hardwick (PC) <bennetthardwick@icloud.com>
2018-03-15 15:14:46 +1000

    Removed terrible tests

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -4,12 +4,5 @@ const port = 3000;
 let server;
 
 describe('Server', () => {
-  it('listens', () => {
-    server = app.listen(port);
-  });
-
-  it('closes', () => {
-    server.close();
-  });
 
 });
