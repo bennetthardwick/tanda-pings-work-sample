4a336a3942ccccefcda8571c7614f78c6093ba61
Bennett Hardwick (PC) <bennetthardwick@icloud.com>
2018-03-15 15:24:58 +1000

    Added tanda tests

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -3,6 +3,24 @@ const expect = require('chai').expect;
 const port = 3000;
 let server;
 
+const exec = require('child_process').exec;
+
 describe('Server', () => {
 
+  beforeEach(() => {
+    server = app.listen(port);
+  });
+
+  afterEach(() => {
+    server.close();
+  });
+
+  it('passes all Tanda tests', (done) => {
+    exec('ruby pings.rb', (err, stdout, stderr) => {
+      if (err) expect(false).to.equal(true);
+      else if (stderr) expect(false.to.equal(true));
+      else expect(true).to.equal(true);
+      done();
+    })
+  })
 });
